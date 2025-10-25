2de19d5918c2a11ebc35b4eed3159e05d2ba07b6
hesham <nbilha161@gmail.com>
2025-10-25 22:32:37 +0300

    feat(chore): integrate with meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Cairo, Amiri, Noto_Sans_Arabic } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const cairo = Cairo({
   variable: "--font-cairo",
@@ -34,8 +35,40 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID!;
+
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        {/* <!-- Meta Pixel Code --> */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${pixelId}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+        {/* <!-- End Meta Pixel Code -->   */}
+      </head>
       <body
         className={`${cairo.variable} ${amiri.variable} ${notoSansArabic.variable} font-cairo antialiased`}
       >
